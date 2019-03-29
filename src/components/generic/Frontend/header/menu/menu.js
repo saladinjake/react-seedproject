d9b0d922967a16dd41bd1559a0e2230d8f889870
kingstone <juwavictor@gmail.com>
2019-03-29 16:12:24 +0000

    Folder structure refactor

--- src/components/generic/Frontend/header/menu/menu.js
+++
@@ -1,9 +0,0 @@
-import NavBarWithLogo from './navbars/navbar_logo.js';
-
-const Menu = ({logo, attachEvents}) => {
-
-	 return(<NavBarWithLogo 
-	 	     logo={logo}
-	 	     attachEvents={attachEvents}
-	 	     />)
-}
